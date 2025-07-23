e4dadbe6f58bacd248f4b8323992a862d49368cd
itsdev04 <imkrdev@gmail.com>
2025-07-23 09:36:17 +0530

    test commit

--- app/src/main/java/devworks/App.java
+++ app/src/main/java/devworks/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringBootConfiguration;
 
 @SpringBootConfiguration
 public class App {
+    /*This is sample comment to check the commit on github*/
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
